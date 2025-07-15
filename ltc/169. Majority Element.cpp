55d124672a8a37447869d70caa7cf6025e78cc02
Ali Mashally <algeserone@yandex.com>
2025-07-15 18:56:39 +0300

    169. Majority Element

---
+++ ltc/169. Majority Element.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        map<int, int> mp;
+        int n = nums.size();
+        int ans = 0;
+        for(int i = 0; i < n; ++i)
+            mp[nums[i]]++;
+
+        int mx = 0;
+        for(int i = 0; i < n; ++i)
+        {
+                if(mx < mp[nums[i]])
+                {
+                    mx = mp[nums[i]];
+                    ans = nums[i];
+                }
+        }
+
+        return ans;
+    }
+};
